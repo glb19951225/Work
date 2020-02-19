f42c717a92ba5c07b8a5a5270791799259e58aad
bb <2443014708@qq.com>
2020-02-19 19:04:30 +0800

    2020/2/19

--- src/main/java/com/nowcoder/community/CommunityServletInitializer.java
+++ src/main/java/com/nowcoder/community/CommunityServletInitializer.java
@@ -11,6 +11,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  */
 public class CommunityServletInitializer extends SpringBootServletInitializer {
 
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(CommunityApplication.class);
